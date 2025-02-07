06b3e746c051ef098ec3457f6cd3b490d0c1c894
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2025-02-07 10:24:31 +0100

    a

--- src/main/java/pl/training/concurrency/ex004_monitor/Application.java
+++ src/main/java/pl/training/concurrency/ex004_monitor/Application.java
@@ -1,12 +1,12 @@
 package pl.training.concurrency.ex004_monitor;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class Application {
 
     private static final int THREADS_COUNT = 3;
     private static final int PRINTING_QUEUE_LIMIT = 100;
+
     public static void main(String[] args) {
         Queue<String> printingQueue = new LinkedList<>();
         new Thread(new Printer(printingQueue)).start();
